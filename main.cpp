014b7d60246635656c412fe9b860f9951b866070
m0rphine1 <107500228+m0rphine1@users.noreply.github.com>
2024-09-29 20:28:01 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
     quadrasoftware w;
     w.setStyleSheet("QMainWindow{border-radius: 12px; border: 1px solid #686D76; background-color:#808080 ;}");
     w.setMinimumSize(QSize(500, 500));
+    w.resize(1500, 900);
     w.show();
     return a.exec();
 }
