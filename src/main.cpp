cf755cb6298db4dfef2914ca9d2d81cdf6d06789
m0rphine1 <107500228+m0rphine1@users.noreply.github.com>
2024-09-30 18:25:50 +0300

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include "quadra_ui.h"
+#include <QtWidgets/QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    quadrasoftware w;
+    w.setStyleSheet("QMainWindow{border-radius: 12px; border: 1px solid #686D76; background-color:#808080 ;}");
+    w.setMinimumSize(QSize(500, 500));
+    w.resize(1500, 900);
+    w.show();
+    return a.exec();
+}
